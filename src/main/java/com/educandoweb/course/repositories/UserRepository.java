e87b432ca6bd484f8b10697ae8583563b9467097
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-04-15 10:58:14 -0300

    Implementado o service layer e o registration component, que tratam inicialmente sobre requisições get para obter todos os usuários do banco e também selecionando por um id específico

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+//como ele já herda de JPA Repository, neste caso não é necessário colocar
+//@repository para indicar que é um repository, mas se colocar também tudo bem
+//não irá dar erro, é opcional. Optei por nao colocar
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
