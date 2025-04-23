14da839dc7d107c4c156217954b2498f9a4b6860
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-04-23 10:40:30 -0300

    Modificações no Order e no OrderItem para cortar a associação de mão dupla

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +36,10 @@ public class Order implements Serializable {
 	@ManyToOne //indica que é (n, 1), vários pedidos para 1 client
 	@JoinColumn(name = "client_id") //indica fk no SGBD
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	
 	public Order() {
 		
@@ -81,6 +88,15 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	
+	
+	public Set<OrderItem> getItems(){
+		return items;
+	}
+	
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,12 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.OrderItem;
+
+//como ele já herda de JPA Repository, neste caso não é necessário colocar
+//@repository para indicar que é um repository, mas se colocar também tudo bem
+//não irá dar erro, é opcional. Optei por nao colocar
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
