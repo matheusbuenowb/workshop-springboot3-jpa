7f31b2ccfa994717abf10ba2fdb310a4f2a0b5cb
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-04-28 11:05:46 -0300

    Adicionados métodos Total e SubTotal em Order e OrderItem, respectivamente

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -107,7 +107,13 @@ public class Order implements Serializable {
 	}
 	
 	
-	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 	
 	@Override
 	public int hashCode() {
