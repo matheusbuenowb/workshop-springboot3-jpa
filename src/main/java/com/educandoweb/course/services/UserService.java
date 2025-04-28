da8a6ee5fe5994d400a1c0ebc61439024092c37f
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-04-28 11:29:08 -0300

    Adicionado o userInsert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,9 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	//salva no sgbd um dado usuário, retorna um usuário salvo
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
