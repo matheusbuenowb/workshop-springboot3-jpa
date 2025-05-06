19a9e18f86b2a7c4839236aed8ae4e14e3b5c459
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-05-06 11:13:53 -0300

    Adicionado tratamento de exceção personalizado para o update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -47,10 +49,16 @@ public class UserService {
 		}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		//prepara o objeto monitorado para modificar e só depois efetuar a operação no banco de dados
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
